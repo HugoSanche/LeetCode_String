fde3155c7be53a96e69fbfb6d4d795e880ea6da7
HugoSanche <hbmclaren@gmail.com>
2023-10-12 23:53:20 -0600

    Add exercise leetcode Maximun number of word 2114

--- LeetCode_String/src/MaximumNumberOfWords.java
+++ LeetCode_String/src/MaximumNumberOfWords.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class MaximumNumberOfWords {
+public class MaximumNumberOfWords {
+    public static void main(String[] args) {
+        String[] sentence= {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+
+        int result=mostWordsFound(sentence);
+        System.out.println(result);
+
+    }
+    public static int mostWordsFound(String[] sentences) {
+        int count=1;
+        int max=0;
+        // StringBuilder[] stringBuilder=new StringBuilder(sentences);
+       // stringBuilder.
+
+        for (int i=0; i<sentences.length; i++){
+
+            int current=sentences[i].split(" ").length;
+            System.out.println(current);
+            for (int j=0; j<sentences[i].length(); j++){
+                if (sentences[i].charAt(j)==' '){
+                    count++;
+                }
+            }
+
+            if (count>max){
+                max=count;
+            }
+            count=1;
+        }
+        return  max;
+    }
 }
