28fb90059531fa604acf4412b7d4082168152b61
HugoSanche <hbmclaren@gmail.com>
2024-04-10 00:07:24 -0600

    add exercise LeetCode 1684. Count the Number of Consistent Strings

--- LeetCode_String/src/CountNumberConsistentStrings.java
+++ LeetCode_String/src/CountNumberConsistentStrings.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class CountNumberConsistentStrings {
+public class CountNumberConsistentStrings {
+    public static void main(String[] args) {
+        String allowed="abc";
+        String[] words={"a","b","c","ab","ac","bc","abc"};
+
+        int result=countConsistentStrings(allowed,words);
+        System.out.println(result);
+
+
+    }
+
+    public static int countConsistentStrings(String allowed, String[] words) {
+        int count=0;
+        for(int i=0; i<words.length; i++){
+            for(int j=0; j<allowed.length(); j++){
+                if(words[i].indexOf(allowed.charAt(j))==-1){
+                   count++;
+                }
+            }
+            //System.out.println("***");
+        }
+        return (words.length-1)-count;
+    }
 }
--- LeetCode_String/src/ToLowerCase.java
+++ LeetCode_String/src/ToLowerCase.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class ToLowerCase {
+//709. To Lower Case
+//Solved
+//Easy
+//Topics
+//Companies
+//Hint
+//Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "Hello"
+//Output: "hello"
+//Example 2:
+//
+//Input: s = "here"
+//Output: "here"
+//Example 3:
+//
+//Input: s = "LOVELY"
+//Output: "lovely"
+//
+//
+//Constraints:
+//
+//1 <= s.length <= 100
+//s consists of printable ASCII characters.
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String s="LOVELY";
+
+
+        String result= toLowerCase(s);
+        System.out.println(result);
+    }
+    public static String toLowerCase(String s) {
+        StringBuilder result=new StringBuilder();
+        char c;
+        for(int i=0; i<s.length();i++){
+
+           if ((int)s.charAt(i)>64 && (int)s.charAt(i)<91 ){
+              // c=(char)((int)s.charAt(i)+32);
+               result.append((char)((int)s.charAt(i)+32));
+           }else{
+              // c=(char)((int)s.charAt(i));
+               result.append((char)((int)s.charAt(i)));
+           }
+           // result.append(c);
+        }
+
+        return result.toString();
+    }
 }
