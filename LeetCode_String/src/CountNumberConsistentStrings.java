f2362f9924b24545d1b1a6380fa9c5359da5b0f9
HugoSanche <hbmclaren@gmail.com>
2024-04-11 18:24:10 -0600

    repair bug for 1684. Count the Number of Consistent Strings

--- LeetCode_String/src/CountNumberConsistentStrings.java
+++ LeetCode_String/src/CountNumberConsistentStrings.java
@@ -1,24 +1,49 @@
 public class CountNumberConsistentStrings {
     public static void main(String[] args) {
-        String allowed="abc";
-        String[] words={"a","b","c","ab","ac","bc","abc"};
+        String allowed="fstqyienx";
+        String[] words={"n","eeitfns","eqqqsfs","i","feniqis","lhoa","yqyitei","sqtn","kug","z","neqqis"};
 
-        int result=countConsistentStrings(allowed,words);
+        //"lhoa"
+        //"kug"
+        //"z"
+
+
+        int result=countConsistentStrings2(allowed,words);
         System.out.println(result);
-
 
     }
 
     public static int countConsistentStrings(String allowed, String[] words) {
         int count=0;
+        String word;
         for(int i=0; i<words.length; i++){
-            for(int j=0; j<allowed.length(); j++){
-                if(words[i].indexOf(allowed.charAt(j))==-1){
+            word=words[i];
+            for(int j=0; j<word.length(); j++){
+                if(allowed.indexOf(word.charAt(j))==-1){
                    count++;
+                   break;
                 }
             }
-            //System.out.println("***");
         }
-        return (words.length-1)-count;
+        return (words.length)-count;
     }
+    public static int countConsistentStrings2(String allowed, String[] words) {
+        int count=words.length;
+        int store[]=new int[26];
+
+        for (char c: allowed.toCharArray()){
+            store[c-'a']=7;
+        }
+
+        for (String word:words){
+            for(char c:word.toCharArray()){
+                if(store[c-'a']!=7){
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
 }
